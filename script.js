99ef19f7f0f2f090d9bec38a7c196b106d7be684
Adamand <dorian.cai@outlook.fr>
2022-07-21 18:19:45 +0200

    javascript fait

--- script.js
+++ script.js
@@ -10,3 +10,92 @@ tour de l’autre joueur.
 Le premier joueur qui atteint les 100 points sur global gagne le jeu.
 
 */
+
+let globalScore = '';
+
+let roundScore = '';
+
+let joueurActif = '';
+
+let tour = '';
+
+
+// Lancement du dé
+
+document.querySelector('.btn-lancer').addEventListener('click', function() {
+   if (tour) {
+        let dice = Math.floor(Math.random() * 6) + 1;
+    
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#resultat-' + joueurActif).textContent = roundScore;
+        } else {
+            prochainJoueur();
+        }    
+    }
+
+
+
+});
+
+
+document.querySelector('.btn-garder').addEventListener('click', function() {
+    if (tour) {
+        globalScore[joueurActif] += roundScore;
+        document.querySelector('score-' + joueurActif).textContent = globalScore[joueurActif];
+
+        if (globalScore[joueurActif] >= 100) {
+            document.querySelector('#nom-' + joueurActif).textContent = 'Vainqueur!';
+            document.querySelector('.dice').style.display ='none';
+            document.querySelector('joueur-' + joueurActif + '-actif').classList.add('vainqueur');
+            document.querySelector('joueur-' + joueurActif + '-actif').classList.remove('actif');
+            tour = false;
+        } else {
+            prochainJoueur()
+        }
+    }
+});
+
+
+function prochainJoueur() {
+    
+ joueurActif === 0 ? joueurActif = 1 : joueurActif = 0;
+    roundScore = 0;
+
+    document.getElementById('resultat-1').textContent = '0';
+    document.getElementById('resultat-2').textContent = '0';
+
+    document.querySelector('.joueur-1').classList.toggle('actif');
+    document.querySelector('.joueur-2').classList.toggle('actif');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-nouvelle-partie').addEventListener('click', init);
+
+function init() {
+    globalScore = [0, 0];
+    joueurActif = 0;
+    roundScore = 0;
+    tour = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('score-2').textContent = '0';
+    document.getElementById('resultat-1').textContent = '0';
+    document.getElementById('resultat-2').textContent = '0';
+    document.getElementById('nom-1').textContent = 'Joueur 1';
+    document.getElementById('nom-2').textContent = 'Joueur 2';
+    document.querySelector('.joueur-1').classList.remove('vainqueur');
+    document.querySelector('.joueur-2').classList.remove('vainqueur');
+    document.querySelector('.joueur-1').classList.remove('actif');
+    document.querySelector('.joueur-2').classList.remove('actif');
+    document.querySelector('.joueur-1').classList.add('actif');
+}
+
+init()
